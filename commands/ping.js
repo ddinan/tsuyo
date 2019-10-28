f72929b9b8d47ba4ae7149877316da6a0f2155a1
K <7ka@criptext.com>
2019-10-28 00:31:03 -0700

    Now that ping works, let's try this again

--- commands/ping.js
+++ commands/ping.js
@@ -9,12 +9,11 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
       .addField(`${message.author.id}`, 'Hello world!');
 
     let msg = await message.channel.send(pingEmbed);
-    let icon = client.guild.iconURL;
 
     let embed = new Discord.RichEmbed()
       .setColor(0x4699AA)
       .setFooter('PONG')
-      .setThumbnail(icon)
+      .setThumbnail(client.guild.iconURL)
 
       .addField('Message Trip',
       `${msg.createdTimestamp - message.createdTimestamp}ms`)
