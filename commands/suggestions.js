80ab45443c882e9262ad8606df4c7d1a3bbac108
VenkSociety <VenkSociety@users.noreply.github.com>
2019-12-18 09:08:39 +1000

    "better" permissions for suggestions command
    Imagine per-role commands :o

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -11,74 +11,87 @@ exports.run = async (client, message, args, level) => {
   } 
 		
 	if (args[0] === "add") {
-		if (args.length === 1) return message.channel.send(`You need to specify the contents of the suggestion.\nE.g, \`${settings.prefix}suggestions add Better syntax descriptions.\``)
+		if (message.member.roles.some(r => r.name === settings.modRole) || message.member.roles.some(r => r.name === settings.adminRole)) {
+			if (args.length === 1) return message.channel.send(`You need to specify the contents of the suggestion.\nE.g, \`${settings.prefix}suggestions add Better syntax descriptions.\``)
 
-		const input = message.content.startsWith(`${settings.prefix}sg add`) ? message.content.split(`${settings.prefix}sg add `) : message.content.split(`${settings.prefix}suggestions add`)
-		
-		const embed = new Discord.RichEmbed()
-			.setAuthor(message.author.tag, message.author.avatarURL)
-			.setColor(colors.default)
-			.setDescription(input)
-			.setTimestamp()
-			.setFooter(`Generating ID...`)
-		
-		const newMsg = await message.channel.send(embed)
-		
-		const newEmbed = new Discord.RichEmbed()
-			.setAuthor(message.author.tag, message.author.avatarURL)
-			.setColor(colors.default)
-			.setDescription(input)
-			.setTimestamp()
-			.setFooter(`ID: ${newMsg.id}`)
+			const input = message.content.startsWith(`${settings.prefix}sg add`) ? message.content.split(`${settings.prefix}sg add `) : message.content.split(`${settings.prefix}suggestions add`)
 
-    newMsg.edit(newEmbed).then(() => {
-			message.delete()
-			newMsg.react(yesEmoji).then(() => {
-				newMsg.react(noEmoji)
+			const embed = new Discord.RichEmbed()
+				.setAuthor(message.author.tag, message.author.avatarURL)
+				.setColor(colors.default)
+				.setDescription(input)
+				.setTimestamp()
+				.setFooter(`Generating ID...`)
+
+			const newMsg = await message.channel.send(embed)
+
+			const newEmbed = new Discord.RichEmbed()
+				.setAuthor(message.author.tag, message.author.avatarURL)
+				.setColor(colors.default)
+				.setDescription(input)
+				.setTimestamp()
+				.setFooter(`ID: ${newMsg.id}`)
+
+			newMsg.edit(newEmbed).then(() => {
+				message.delete()
+				newMsg.react(yesEmoji).then(() => {
+					newMsg.react(noEmoji)
+					return
+				})
 			})
-		})
+		} else { return message.channel.send("You do not have permission to use this command.") }
 	}
 	
 	if (args[0] === "delete" || args[0] === "del" || args[0] === "deny" || args[0] === "decline") {
-		if (args.length === 1) return message.channel.send('You need to specify a suggestion to deny.')
-		
-		if (settings.deniedChannel && message.channel.guild.channels.find(c => c.name == settings.deniedChannel)) {
-      const deniedChannel = message.channel.guild.channels.find(c => c.name == settings.deniedChannel)
+		if (message.member.roles.some(r => r.name === settings.adminRole)) {
+			if (args.length === 1) return message.channel.send('You need to specify a suggestion to deny.')
 
-			message.channel.fetchMessage(args[1])
-  			.then(suggestion => { 
-					const embed = suggestion.embeds[0];
-					const newEmbed = new Discord.RichEmbed(embed)
-						.setColor(colors.red)
-						.setTimestamp()
-						.setFooter(`ID: ${args[1]}`)
-					
-					deniedChannel.send(newEmbed)
-					suggestion.delete()
-					message.delete()
-				}).catch(message.channel.send("Invalid message ID."))
+			if (settings.deniedChannel && message.channel.guild.channels.find(c => c.name == settings.deniedChannel)) {
+				const deniedChannel = message.channel.guild.channels.find(c => c.name == settings.deniedChannel)
+
+				message.channel.fetchMessage(args[1])
+					.then(suggestion => { 
+						const embed = suggestion.embeds[0];
+						const newEmbed = new Discord.RichEmbed(embed)
+							.setColor(colors.red)
+							.setTimestamp()
+							.setFooter(`ID: ${args[1]}`)
+
+						deniedChannel.send(newEmbed)
+						suggestion.delete()
+						message.delete()
+						return
+					}).catch()
+			}
 		}
+		
+		else { return message.channel.send("You do not have permission to use this command.") }
 	}
 	
 	if (args[0] === "accept") {
-		if (args.length === 1) return message.channel.send('You need to specify a suggestion to accept.')
-		
-		if (settings.acceptedChannel && message.channel.guild.channels.find(c => c.name == settings.acceptedChannel)) {
-      const acceptedChannel = message.channel.guild.channels.find(c => c.name == settings.acceptedChannel)
+		if (message.member.roles.some(r => r.name === settings.adminRole)) {
+			if (args.length === 1) return message.channel.send('You need to specify a suggestion to accept.')
 
-			message.channel.fetchMessage(args[1])
-  			.then(suggestion => { 
-					const embed = suggestion.embeds[0];
-					const newEmbed = new Discord.RichEmbed(embed)
-						.setColor(colors.green)
-						.setTimestamp()
-						.setFooter(`ID: ${args[1]}`)
-					
-					acceptedChannel.send(newEmbed)
-					suggestion.delete()
-					message.delete()
-				}).catch(message.channel.send("Invalid message ID."))
+			if (settings.acceptedChannel && message.channel.guild.channels.find(c => c.name == settings.acceptedChannel)) {
+				const acceptedChannel = message.channel.guild.channels.find(c => c.name == settings.acceptedChannel)
+
+				message.channel.fetchMessage(args[1])
+					.then(suggestion => { 
+						const embed = suggestion.embeds[0];
+						const newEmbed = new Discord.RichEmbed(embed)
+							.setColor(colors.green)
+							.setTimestamp()
+							.setFooter(`ID: ${args[1]}`)
+
+						acceptedChannel.send(newEmbed)
+						suggestion.delete()
+						message.delete()
+						return
+					}).catch()
+			}
 		}
+		
+		else { return message.channel.send("You do not have permission to use this command.") }
 	}
 }
 
