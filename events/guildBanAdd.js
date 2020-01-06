6f8a0188f818a5ea7a5a5e71b01e9df280fd6214
VenkSociety <VenkSociety@users.noreply.github.com>
2020-01-06 10:53:10 +1000

    Small fixes after last commit

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -3,24 +3,24 @@ const colors = require('../lib/colors.json')
 
 module.exports = (client, guild, user) => {
   const settings = client.getSettings(guild.id)
-  const modLogChannel guild.channels.find(c => c.name === settings.modLogChannel)
+  const modLogChannel = guild.channels.find(c => c.name === settings.modLogChannel)
   
-  if (message.author.bot) return
-  if (message.guild === null) return
+  if (guild === null) return
 
   if (settings.logMessageUpdates == 'true') {
-	if (settings.modLogChannel && message.guild.channels.find(c => c.name == settings.modLogChannel)) {
-	  const modLogChannel = message.guild.channels.find(c => c.name == settings.modLogChannel)
-	  if (!modLogChannel.permissionsFor(message.guild.me).has('VIEW_CHANNEL')) return
-	  if (!modLogChannel.permissionsFor(message.guild.me).has('SEND_MESSAGES')) return
+	if (settings.modLogChannel && guild.channels.find(c => c.name == settings.modLogChannel)) {
+	  const modLogChannel = guild.channels.find(c => c.name == settings.modLogChannel)
+	  if (!modLogChannel.permissionsFor(guild.me).has('VIEW_CHANNEL')) return
+	  if (!modLogChannel.permissionsFor(guild.me).has('SEND_MESSAGES')) return
 
 	  const embed = new Discord.RichEmbed()
         .setTitle('🔨 Member banned')
         .setColor(colors.red)
-        .setDescription(`**Total member count:** \`${member.guild.memberCount}\`\n<@${user.id}> was banned from the Discord.`)
+        .setDescription(`**Total member count:** \`${guild.memberCount}\`\n<@${user.id}> was banned from the Discord.`)
         .setThumbnail(user.displayAvatarURL)
         .setTimestamp()
 
 		modLogChannel.send(embed)
-	}
+    }
+  }
 }
