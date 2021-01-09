4c84ec5da01c3db4d5d1988d83e0ed20750eded0
Derek <derekdinan@users.noreply.github.com>
2021-01-09 16:30:23 +1000

    Fix ;;ping not returning valid time

--- commands/ping.js
+++ commands/ping.js
@@ -2,42 +2,38 @@ const Discord = require('discord.js')
 const colors = require('../lib/colors.json')
 
 exports.run = async (client, message, args, level) => {
-  try {
-    const pingEmbed = new Discord.MessageEmbed()
-      .setColor(colors.default)
-      .setFooter('PING')
-      .addField(`${message.author.id}`, 'Hello world!')
+    try {
+        const pingEmbed = new Discord.MessageEmbed()
+            .setColor(colors.default)
+            .setFooter('PING')
+            .addField(`${message.author.id}`, 'Hello world!')
 
-    const msg = await message.channel.send(pingEmbed)
+        const msg = await message.channel.send(pingEmbed)
 
-    const embed = new Discord.MessageEmbed()
-      .setColor(colors.default)
-      .setFooter('PONG',
-        'https://cdn.discordapp.com/avatars/492871769485475840/6164d0068b8e76e497af9b0e1746f671.png?size=2048')
+        const embed = new Discord.MessageEmbed()
+            .setColor(colors.default)
+            .setFooter('PONG',
+                'https://cdn.discordapp.com/avatars/492871769485475840/29ea7a72135ebde63f8cebd5f82ef42b.webp')
 
-      .addField('Message Trip',
-      `${msg.createdTimestamp - message.createdTimestamp}ms`)
-      .addField('WebSocket\nHeartbeat',
-      `${Math.floor(client.pings[0])}ms`, true)
-      .addField('Average WebSocket\nHeartbeat',
-      `${Math.floor(client.pings.average())}ms`, true)
+            .addField('Message Trip',
+                `${msg.createdTimestamp - message.createdTimestamp}ms`)
 
-    msg.edit(embed)
-  } catch (err) {
-    message.channel.send(client.errors.genericError + err.stack).catch();
-  }
+        msg.edit(embed)
+    } catch (err) {
+        message.channel.send(client.errors.genericError + err.stack).catch();
+    }
 }
 
 exports.conf = {
-  enabled: true,
-  aliases: [],
-  guildOnly: false,
-  permLevel: 'User'
+    enabled: true,
+    aliases: [],
+    guildOnly: false,
+    permLevel: 'User'
 }
 
 exports.help = {
-  name: 'ping',
-  category: 'Utility',
-  description: 'Returns your ping.',
-  usage: 'ping'
+    name: 'ping',
+    category: 'Utility',
+    description: 'Returns your ping.',
+    usage: 'ping'
 }
