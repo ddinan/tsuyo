5cffebd9b78d08d5678dd21bbd3dc671af3029c1
Derek <derekdinan@users.noreply.github.com>
2021-03-28 08:02:43 +1000

    Fix ;;marry deprecation issues

--- commands/marry.js
+++ commands/marry.js
@@ -1,94 +1,98 @@
-﻿const Discord = require('discord.js')
+const Discord = require('discord.js')
 const colors = require('../lib/colors.json')
 
 exports.run = async (client, message, args) => {
-  try {
-    const user = message.mentions.users.first() || client.users.get(args[0])
-    if (!user) return message.channel.send('You must mention someone or give their ID!')
-    if (user.bot === true) return message.channel.send('Don\'t marry bots. They have no feelings... trust me...')
-    if (user === message.author || message.author.id === user.id) return message.channel.send('It really do be like that sometimes...')
+    try {
+        const user = message.mentions.users.first() || client.users.cache.get(args[0])
+        if (!user) return message.channel.send('You must mention someone or give their ID!')
+        if (user.bot === true) return message.channel.send('Don\'t marry bots. They have no feelings... trust me...')
+        if (user === message.author || message.author.id === user.id) return message.channel.send('It really do be like that sometimes...')
 
-    let proposerID = message.author.id
-    let proposerName = message.author.username
+        let proposerID = message.author.id
+        let proposerName = message.author.username
 
-    client.life.ensure(user.id, {
-      member: user.id,
-      spouse: 0,
-      job: 0
-    })
-
-    client.life.ensure(message.author.id, {
-      member: message.author.id,
-      spouse: 0,
-      job: 0
-    })
-
-    client.inventory.ensure(message.author.id, {
-      member: message.author.id,
-      rings: 0,
-      petfood: 0,
-      seeds: 0,
-    })
-
-    const rings = client.inventory.get(message.author.id, 'rings')
-    const spouse = client.life.get(message.author.id, 'spouse')
-    const uSpouse = client.life.get(user.id, 'spouse')
-
-    if (rings === 0) return message.channel.send('You do not have a wedding ring. Buy one in the shop.')
-    if (spouse !== 0) return message.channel.send('You cannot have more than one spouse.')
-    if (uSpouse !== 0) return message.channel.send(`${user.tag} already has a spouse.`)
-
-    let embed = new Discord.MessageEmbed()
-      .setDescription(`**${user.tag}**, **${message.author.tag}** is asking for your hand in marriage, would you like to accept?`)
-
-    const noEmoji = message.client.emojis.cache.get('637573919204966410')
-    message.channel.send(embed).then(message => {
-      message.react('✅').then(() => message.react(noEmoji));
-
-      const filter = (reaction, sent) => {
-        return ['✅', noEmoji].includes(reaction.emoji.name) && sent.id === user.id;
-      };
-client.channels.cache.find(ch => ch.id === args[0])
-    const proposer = message.guild.members.cache.find(p => p.id === proposerID)
-
-      message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-        .then(collected => {
-          const reaction = collected.first()
-
-          if (reaction.emoji.name === '✅') {
-            client.life.set(proposerID, user.id, 'spouse')
-            client.life.set(user.id, proposerID, 'spouse')
-            client.inventory.set(proposerID, rings - 1, 'rings')
-
-            embed.setDescription(`${user.tag} and ${proposer.user.tag} are now married`)
-              .setImage('https://media.giphy.com/media/vTfFCC3rSfKco/giphy.gif')
-              .setColor(colors.pink)
-            message.channel.send(embed)
-          }
-          if (reaction.emoji.id === '637573919204966410') { // Decline emoji
-            embed.setTitle(`Sorry **${proposer.user.tag}**, **${user.tag}** declined your proposal.`)
-            message.edit(embed)
-          }
+        client.life.ensure(user.id, {
+            member: user.id,
+            spouse: 0,
+            job: 0
         })
-        .catch(collected => {
-          message.channel.send(`Sorry ${proposer.user.tag}, the person you proposed to didn't respond, try again later.`)
-      });
-    })
-  } catch (err) {
-    message.channel.send(client.errors.genericError + err.stack).catch();
-  }
+
+        client.life.ensure(message.author.id, {
+            member: message.author.id,
+            spouse: 0,
+            job: 0
+        })
+
+        client.inventory.ensure(message.author.id, {
+            member: message.author.id,
+            rings: 0,
+            petfood: 0,
+            seeds: 0,
+        })
+
+        const rings = client.inventory.get(message.author.id, 'rings')
+        const spouse = client.life.get(message.author.id, 'spouse')
+        const uSpouse = client.life.get(user.id, 'spouse')
+
+        if (rings === 0) return message.channel.send('You do not have a wedding ring. Buy one in the shop.')
+        if (spouse !== 0) return message.channel.send('You cannot have more than one spouse.')
+        if (uSpouse !== 0) return message.channel.send(`${user.tag} already has a spouse.`)
+
+        let embed = new Discord.MessageEmbed()
+            .setDescription(`**${user.tag}**, **${message.author.tag}** is asking for your hand in marriage, would you like to accept?`)
+
+        const noEmoji = message.client.emojis.cache.get('637573919204966410')
+        message.channel.send(embed).then(message => {
+            message.react('✅').then(() => message.react(noEmoji));
+
+            const filter = (reaction, sent) => {
+                return ['✅', noEmoji].includes(reaction.emoji.name) && sent.id === user.id;
+            };
+            client.channels.cache.find(ch => ch.id === args[0])
+            const proposer = message.guild.members.cache.find(p => p.id === proposerID)
+
+            message.awaitReactions(filter, {
+                    max: 1,
+                    time: 60000,
+                    errors: ['time']
+                })
+                .then(collected => {
+                    const reaction = collected.first()
+
+                    if (reaction.emoji.name === '✅') {
+                        client.life.set(proposerID, user.id, 'spouse')
+                        client.life.set(user.id, proposerID, 'spouse')
+                        client.inventory.set(proposerID, rings - 1, 'rings')
+
+                        embed.setDescription(`${user.tag} and ${proposer.user.tag} are now married`)
+                            .setImage('https://media.giphy.com/media/vTfFCC3rSfKco/giphy.gif')
+                            .setColor(colors.pink)
+                        message.channel.send(embed)
+                    }
+                    if (reaction.emoji.id === '637573919204966410') { // Decline emoji
+                        embed.setTitle(`Sorry **${proposer.user.tag}**, **${user.tag}** declined your proposal.`)
+                        message.edit(embed)
+                    }
+                })
+                .catch(collected => {
+                    message.channel.send(`Sorry ${proposer.user.tag}, the person you proposed to didn't respond, try again later.`)
+                });
+        })
+    } catch (err) {
+        message.channel.send(client.errors.genericError + err.stack).catch();
+    }
 }
 
 exports.conf = {
-  enabled: true,
-  aliases: ['propose', 'preparetoruinyourlife'],
-  guildOnly: true,
-  permLevel: 'User'
+    enabled: true,
+    aliases: ['propose', 'preparetoruinyourlife'],
+    guildOnly: true,
+    permLevel: 'User'
 }
 
 exports.help = {
-  name: 'marry',
-  category: 'Fun',
-  description: 'Proposes to <member>.',
-  usage: 'marry <member>'
+    name: 'marry',
+    category: 'Fun',
+    description: 'Proposes to <member>.',
+    usage: 'marry <member>'
 }
